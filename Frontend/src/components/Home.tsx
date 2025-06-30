9130f7c05ba377667eeeffb186c0580d3cc34125
Umesh Jadhav <163861510+UmeshpJadhav@users.noreply.github.com>
2025-06-30 23:43:21 +0530

    Update Home.tsx

--- Frontend/src/components/Home.tsx
+++ Frontend/src/components/Home.tsx
@@ -6,7 +6,7 @@ const HomePage: React.FC = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-200">
       <div className="bg-white flex items-center flex-col p-8 rounded-lg shadow-md w-full max-w-xs">
-        <h1 className="text-2xl font-bold mb-4 text-center">Welcome to Omegle Clone</h1>
+        <h1 className="text-2xl font-bold mb-4 text-center">Welcome to AnoneConnect</h1>
         <Link 
           to="/chat" 
           className="w-full text-center bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700"
